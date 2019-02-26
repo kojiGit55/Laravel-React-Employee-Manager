45a9fc9652f65c4265a0ae439add596cfc1ffdae
nakashima <nakashima1711@gmail.com>
2019-02-26 22:24:10 +0900

    社員一覧画面作成

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-export default class Example extends Component {
+
+export default class EmployeeList extends Component {
     render() {
         return (
             <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-
-                            <div className="card-body">
-                                I'm an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>社員番号</td>
+                            <td>名前</td>
+                            <td>性別</td>
+                            <td>勤続年数</td>
+                            <td>職種</td>
+                            <td>役職</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>A山A太郎</td>
+                            <td>男性</td>
+                            <td>5年</td>
+                            <td>開発部</td>
+                            <td>部長</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>A山A太郎</td>
+                            <td>男性</td>
+                            <td>5年</td>
+                            <td>開発部</td>
+                            <td>部長</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
 
 if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+    ReactDOM.render(<EmployeeList />, document.getElementById('example'));
 }
